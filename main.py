9011b853a81b32f71ec4c43f868111df12e8550a
Gustav Sönnichsen <gustavsoennichsen@icloud.com>
2026-02-12 14:00:07 -0400

    Added new Braille Table

--- main.py
+++ main.py
@@ -1,44 +1,67 @@
-BRAILLE_MAP = {
-    ' ': {'unicode':'U+2800','dot_position_code':'000000'},
-    'a': {'unicode':'U+2801','dot_position_code':'100000'},
-    'b': {'unicode':'U+2803','dot_position_code':'110000'},
-    'c': {'unicode':'U+2809','dot_position_code':'100100'},
-    'd': {'unicode':'U+2819','dot_position_code':'100110'},
-    'e': {'unicode':'U+2811','dot_position_code':'100010'},
-    'f': {'unicode':'U+280B','dot_position_code':'110100'},
-    'g': {'unicode':'U+281B','dot_position_code':'110110'},
-    'h': {'unicode':'U+2813','dot_position_code':'110010'},
-    'i': {'unicode':'U+280A','dot_position_code':'010100'},
-    'j': {'unicode':'U+281A','dot_position_code':'010110'},
-    'k': {'unicode':'U+2805','dot_position_code':'101000'},
-    'l': {'unicode':'U+2807','dot_position_code':'111000'},
-    'm': {'unicode':'U+280D','dot_position_code':'101100'},
-    'n': {'unicode':'U+281D','dot_position_code':'101110'},
-    'o': {'unicode':'U+2815','dot_position_code':'101010'},
-    'p': {'unicode':'U+280F','dot_position_code':'111100'},
-    'q': {'unicode':'U+281F','dot_position_code':'111110'},
-    'r': {'unicode':'U+2817','dot_position_code':'111010'},
-    's': {'unicode':'U+280E','dot_position_code':'011100'},
-    't': {'unicode':'U+281E','dot_position_code':'011110'},
-    'u': {'unicode':'U+2825','dot_position_code':'101001'},
-    'v': {'unicode':'U+2827','dot_position_code':'111001'},
-    'w': {'unicode':'U+283A','dot_position_code':'010111'},
-    'x': {'unicode':'U+282D','dot_position_code':'101101'},
-    'y': {'unicode':'U+283D','dot_position_code':'101111'},
-    'z': {'unicode':'U+2835','dot_position_code':'101011'}
+braille_table = {
+
+    # Letters
+    "a": {"unicode": "⠁", "bin": "100000"},
+    "b": {"unicode": "⠃", "bin": "110000"},
+    "c": {"unicode": "⠉", "bin": "100100"},
+    "d": {"unicode": "⠙", "bin": "100110"},
+    "e": {"unicode": "⠑", "bin": "100010"},
+    "f": {"unicode": "⠋", "bin": "110100"},
+    "g": {"unicode": "⠛", "bin": "110110"},
+    "h": {"unicode": "⠓", "bin": "110010"},
+    "i": {"unicode": "⠊", "bin": "010100"},
+    "j": {"unicode": "⠚", "bin": "010110"},
+    "k": {"unicode": "⠅", "bin": "101000"},
+    "l": {"unicode": "⠇", "bin": "111000"},
+    "m": {"unicode": "⠍", "bin": "101100"},
+    "n": {"unicode": "⠝", "bin": "101110"},
+    "o": {"unicode": "⠕", "bin": "101010"},
+    "p": {"unicode": "⠏", "bin": "111100"},
+    "q": {"unicode": "⠟", "bin": "111110"},
+    "r": {"unicode": "⠗", "bin": "111010"},
+    "s": {"unicode": "⠎", "bin": "011100"},
+    "t": {"unicode": "⠞", "bin": "011110"},
+    "u": {"unicode": "⠥", "bin": "101001"},
+    "v": {"unicode": "⠧", "bin": "111001"},
+    "w": {"unicode": "⠺", "bin": "010111"},
+    "x": {"unicode": "⠭", "bin": "101101"},
+    "y": {"unicode": "⠽", "bin": "101111"},
+    "z": {"unicode": "⠵", "bin": "101011"},
+
+    # Numbers (same shapes as a–j)
+    "1": {"unicode": "⠁", "bin": "100000"},
+    "2": {"unicode": "⠃", "bin": "110000"},
+    "3": {"unicode": "⠉", "bin": "100100"},
+    "4": {"unicode": "⠙", "bin": "100110"},
+    "5": {"unicode": "⠑", "bin": "100010"},
+    "6": {"unicode": "⠋", "bin": "110100"},
+    "7": {"unicode": "⠛", "bin": "110110"},
+    "8": {"unicode": "⠓", "bin": "110010"},
+    "9": {"unicode": "⠊", "bin": "010100"},
+    "0": {"unicode": "⠚", "bin": "010110"},
+
+    # Special signs
+    #"number_sign": {"unicode": "⠼", "bin": "001111"},
+    #"capital_sign": {"unicode": "⠠", "bin": "000001"},
+    " ": {"unicode": "⠀", "bin": "000000"},
+
+    # Punctuation
+    ".": {"unicode": "⠲", "bin": "010011"},
+    ",": {"unicode": "⠂", "bin": "010000"},
+    ";": {"unicode": "⠆", "bin": "011000"},
+    ":": {"unicode": "⠒", "bin": "010010"},
+    "?": {"unicode": "⠦", "bin": "011001"},
+    "!": {"unicode": "⠖", "bin": "011010"},
+    "'": {"unicode": "⠄", "bin": "001000"},
+    "-": {"unicode": "⠤", "bin": "001001"},
+    "/": {"unicode": "⠌", "bin": "001100"}
 }
-
-def unicode2char(uni):
-    return chr(int(uni.replace('U+',''),16))
-
-def unicode2dot_position_code(uni):
-    return bin(int(uni.replace('U+',''),16) - 0x2800)[2:].zfill(6)
-
-
-txt = "The quick brown fox jumps over the lazy dog"
+Csv_out = []
 braille = ""
-for i in range(0,len(txt)):
-    print(unicode2dot_position_code(BRAILLE_MAP[txt[i].lower()]['unicode']))
-    braille = braille + unicode2char(BRAILLE_MAP[txt[i].lower()]['unicode'])
+txt = "The quick brown fox jumps over the lazy dog."
+for char in txt:
+    char = char.lower()
+    braille = braille + braille_table[char]["unicode"]
+
 
 print(braille)
