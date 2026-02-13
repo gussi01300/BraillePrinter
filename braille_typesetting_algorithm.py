b6ca455425be3539426afe470d5b43caf2175df6
Gustav Sönnichsen <gustavsoennichsen@icloud.com>
2026-02-13 15:02:07 -0400

    Refactor coordinate calculation into a separate function

--- braille_typesetting_algorithm.py
+++ braille_typesetting_algorithm.py
@@ -1,8 +1,11 @@
 from pagespec import PageSpec
 
 specs = PageSpec()
+def calc_coordinates():
+    for line in range(specs.max_lines):
+      for col in range(specs.max_cols):
+        coordinate = (col, line)
+        print(coordinate)
 
-for line in range(specs.max_lines):
-  for col in range(specs.max_cols):
-    coordinate = (col, line)
-    print(coordinate)
+print(specs.max_cols, specs.max_lines)
+#Uncompleted, I forgot linebreak rules
