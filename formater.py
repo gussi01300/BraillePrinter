3e699b6eaff5eb376c2c8782ba36678fc948f7ee
Gustav Sönnichsen <gustavsoennichsen@icloud.com>
2026-02-24 13:56:01 -0400

    Refactor braille_dot_position to accept cell dimensions as parameters; update main function to handle file input and improve file type detection

--- formater.py
+++ formater.py
@@ -1,10 +1,9 @@
 from pagespec import PageSpec
-from typing import List
+from typing import List, Dict
 from braille_typesetting_algorithm import Positioned_Cell
 
 
 def format(cells: List[Positioned_Cell]) -> List:
-
     pages_dict = {}
     specs = PageSpec()
     for i in cells:
@@ -13,8 +12,15 @@ def format(cells: List[Positioned_Cell]) -> List:
             pages_dict[i.page] = []
         
         x_mm, y_mm = specs.cell_to_mm(i.col, i.row)
-        pages_dict[i.page].append((x_mm, y_mm, i.Cell_info.unicode))
+        pages_dict[i.page].append((i.Cell_info.dots, x_mm, y_mm))
 
-    return pages_dict
+    out = dict_to_list(pages_dict)
 
+    return out
 
+def dict_to_list(Cell_dict: Dict) -> List:
+    outline_list = []
+    for values in Cell_dict.values():
+        outline_list.append(values)
+
+    return outline_list
